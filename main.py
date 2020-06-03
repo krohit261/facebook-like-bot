ec2e771a38789f9c487b1b13f89fb382cc5d05b2
skcraaze <skcraaze@gmail.com>
2020-06-03 16:12:50 -0700

    removed space from procfile

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class facebook():
 
     def __init__(self):
         self.browser = webdriver.PhantomJS()
-        self.browser.set_window_size(1120, 550)
+        self.browser.set_window_size(1120, 1120)
         
     def login(self):
         username = os.environ.get('fbuser')
