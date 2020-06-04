175a9c473c372f5c7c66c6994a7b690033674029
skcraaze <skcraaze@gmail.com>
2020-06-03 19:25:33 -0700

    added screenshot feature

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from selenium.common.exceptions import ElementNotInteractableException
 class facebook():
 
     def __init__(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.PhantomJS()
         self.browser.set_window_size(1120, 1120)
         
     def login(self):
